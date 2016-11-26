36a169bd145395427ce09737ef042ffe647f22f8
Sylvan Mostert <smostert.dev@gmail.com>
2016-11-26 16:31:04 -0500

    defined updateGameList and updateStreamList in each fragment.
    Removed old drawer from BrowseAcitivity and added new drawer.
    
    Removed onResume override
    
    Added the loading fragment to launch before a fragment and removed on list data change.
    
    Moved main listview code from BrowseAcitivity to each corresponding Fragments.

--- app/src/main/java/teamsylvanmatthew/memecenter/BrowseActivity.java
+++ app/src/main/java/teamsylvanmatthew/memecenter/BrowseActivity.java
@@ -1,6 +1,5 @@
 package teamsylvanmatthew.memecenter;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -12,42 +11,14 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-
-import com.mb3364.http.RequestParams;
-import com.mb3364.twitch.api.Twitch;
-import com.mb3364.twitch.api.handlers.StreamsResponseHandler;
-import com.mb3364.twitch.api.handlers.TopGamesResponseHandler;
-import com.mb3364.twitch.api.models.Stream;
-import com.mb3364.twitch.api.models.TopGame;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class BrowseActivity extends AppCompatActivity {
     private static final String TAG = "BrowseActivity";
-    private Twitch twitch;
 
     private DrawerLayout mDrawerLayout;
     private Toolbar toolbar;
     private NavigationView navigationView;
-    private ActionBarDrawerToggle drawerToggle;
-
-    private LinearLayout mDrawerLinear;
-    private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
-    private ListView browseListView;
-    private TopGameAdapter mGameAdapter;
-    private StreamAdapter mStreamAdapter;
-    private ArrayAdapter<String> mFollowingAdapter;
-    private ArrayList<TopGame> gameList;
-    private ArrayList<Stream> streamList;
-    private List<String> followingList;
-    private int currentItem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,22 +29,12 @@ public class BrowseActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        mDrawerToggle = setupDrawerToggle();
         navigationView = (NavigationView) findViewById(R.id.navigation);
+
         setupDrawer(navigationView);
-
-
-
-        twitch = new Twitch();
-        String apikey = getResources().getString(R.string.apikey);
-        twitch.setClientId(apikey);
-
-
-        `mDrawerList = (ListView) findViewById(R.id.drawer_list);
-
-        `setupDrawer();
-
-        `getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        `getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
@@ -82,6 +43,10 @@ public class BrowseActivity extends AppCompatActivity {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
+        }
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -98,20 +63,31 @@ public class BrowseActivity extends AppCompatActivity {
     }
 
     public void selectDrawerItem(MenuItem menuItem) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
         Fragment fragment = null;
         Class fragmentClass;
+
+        fragmentClass = LoadingFragment.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        fragmentManager.beginTransaction().replace(R.id.fragmentLayout, fragment, "TAG_LOADING").commit();
+
         switch (menuItem.getItemId()) {
-            case R.id.nav_first_fragment:
-                fragmentClass = FirstFragment.class;
+            case R.id.nav_game:
+                fragmentClass = GameFragment.class;
                 break;
-            case R.id.nav_second_fragment:
-                fragmentClass = SecondFragment.class;
+            case R.id.nav_stream:
+                fragmentClass = StreamFragment.class;
                 break;
-            case R.id.nav_third_fragment:
-                fragmentClass = ThirdFragment.class;
+            case R.id.nav_follow:
+                fragmentClass = LoadingFragment.class;
                 break;
             default:
-                fragmentClass = FirstFragment.class;
+                fragmentClass = GameFragment.class;
         }
 
         try {
@@ -120,41 +96,16 @@ public class BrowseActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragmentLayout, fragment).commit();
+
+        fragmentManager.beginTransaction().add(R.id.fragmentLayout, fragment).commit();
 
         menuItem.setChecked(true);
         setTitle(menuItem.getTitle());
         mDrawerLayout.closeDrawers();
     }
 
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        setupBrowseList();
-    }
-
-
-    private void setupBrowseList() {
-        gameList = new ArrayList<TopGame>();
-        streamList = new ArrayList<Stream>();
-        followingList = new ArrayList<String>();
-
-        browseListView = (ListView) findViewById(R.id.browseListView);
-
-        mGameAdapter = new TopGameAdapter(this, gameList);
-        mStreamAdapter = new StreamAdapter(this, streamList);
-        mFollowingAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, followingList);
-
-        updateGamesList();
-        browseListView.setAdapter(mGameAdapter);
-    }
-
-    private void selectItem(int position) {
-        mDrawerList.setItemChecked(position, true);
-        mDrawerLayout.closeDrawer(mDrawerLinear);
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
     }
 
     @Override
@@ -168,126 +119,4 @@ public class BrowseActivity extends AppCompatActivity {
         super.onConfigurationChanged(newConfig);
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
-
-    private void updateGamesList() {
-
-        RequestParams params = new RequestParams();
-        params.put("limit", 5);
-
-        TopGamesResponseHandler topGamesResponseHandler = new TopGamesResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, List<TopGame> topGames) {
-                /* Successful response from the Twitch API */
-                gameList.clear();
-
-                for (TopGame topGame : topGames) {
-                    gameList.add(topGame);
-                }
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        mGameAdapter.notifyDataSetChanged();
-                    }
-                });
-            }
-
-            @Override
-            public void onFailure(int statusCode, String statusMessage, String errorMessage) {
-                /* Twitch API responded with an error message */
-            }
-
-            @Override
-            public void onFailure(Throwable e) {
-                /* Unable to access Twitch, or error parsing the response */
-            }
-        };
-
-        twitch.games().getTop(params, topGamesResponseHandler);
-    }
-
-    private void updateStreamList() {
-
-        RequestParams params = new RequestParams();
-        params.put("limit", 5);
-
-        StreamsResponseHandler streamsResponseHandler = new StreamsResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, List<Stream> streams) {
-                /* Successful response from the Twitch API */
-                streamList.clear();
-
-                for (Stream stream : streams) {
-                    streamList.add(stream);
-                }
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        mStreamAdapter.notifyDataSetChanged();
-                    }
-                });
-            }
-
-            @Override
-            public void onFailure(int statusCode, String statusMessage, String errorMessage) {
-                /* Twitch API responded with an error message */
-            }
-
-            @Override
-            public void onFailure(Throwable e) {
-                /* Unable to access Twitch, or error parsing the response */
-            }
-
-        };
-
-        twitch.streams().get(params, streamsResponseHandler);
-    }
-
-    private boolean updateFollowingList() {
-        followingList.clear();
-        followingList.add("Following 1");
-        followingList.add("Following 2");
-        followingList.add("Following 3");
-
-        return true;
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            //Toast.makeText(BrowseActivity.this, "Item " + position, Toast.LENGTH_SHORT).show();
-            currentItem = position;
-
-            switch (position) {
-                case 0:
-                    updateGamesList();
-                    browseListView.setAdapter(mGameAdapter);
-                    break;
-                case 1:
-                    updateStreamList();
-                    browseListView.setAdapter(mStreamAdapter);
-                    break;
-                case 2:
-                    updateFollowingList();
-                    browseListView.setAdapter(mFollowingAdapter);
-                    break;
-                default:
-                    updateGamesList();
-                    browseListView.setAdapter(mGameAdapter);
-            }
-
-            selectItem(position);
-        }
-    }
-
-    private class StreamItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            Intent chatIntent = new Intent(BrowseActivity.this, ChatActivity.class);
-            startActivity(chatIntent);
-
-            selectItem(position);
-        }
-    }
-
 }
--- app/src/main/java/teamsylvanmatthew/memecenter/StreamFragment.java
+++ app/src/main/java/teamsylvanmatthew/memecenter/StreamFragment.java
@@ -1,17 +1,94 @@
 package teamsylvanmatthew.memecenter;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.mb3364.http.RequestParams;
+import com.mb3364.twitch.api.Twitch;
+import com.mb3364.twitch.api.handlers.StreamsResponseHandler;
+import com.mb3364.twitch.api.models.Stream;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StreamFragment extends Fragment {
+    private Twitch twitch;
+    private Activity mActivity;
+    private StreamAdapter mStreamAdapter;
+    private ArrayList<Stream> streamList;
+    private ListView streamListView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragmentLayout = inflater.inflate(R.layout.fragment_stream, container, false);
+        mActivity = getActivity();
+
+        twitch = new Twitch();
+        String apikey = getResources().getString(R.string.apikey);
+        twitch.setClientId(apikey);
+
+        streamListView = (ListView) fragmentLayout.findViewById(R.id.stream_listview);
+        setupStreamList();
+
         return fragmentLayout;
     }
 
+    private void setupStreamList() {
+        streamList = new ArrayList<Stream>();
+        mStreamAdapter = new StreamAdapter(mActivity, streamList);
+
+        updateStreamList();
+        streamListView.setAdapter(mStreamAdapter);
+    }
+
+    private void updateStreamList() {
+
+        RequestParams params = new RequestParams();
+        params.put("limit", 5);
+
+        StreamsResponseHandler streamsResponseHandler = new StreamsResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, List<Stream> streams) {
+                /* Successful response from the Twitch API */
+                streamList.clear();
+
+                for (Stream stream : streams) {
+                    streamList.add(stream);
+                }
+
+                mActivity.runOnUiThread(new Runnable() {
+                    public void run() {
+                        mStreamAdapter.notifyDataSetChanged();
+
+                        FragmentManager fragmentManager = getFragmentManager();
+                        Fragment loadingFragment = fragmentManager.findFragmentByTag("TAG_LOADING");
+
+                        if (loadingFragment != null) {
+                            fragmentManager.beginTransaction().remove(loadingFragment).commit();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(int statusCode, String statusMessage, String errorMessage) {
+                /* Twitch API responded with an error message */
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                /* Unable to access Twitch, or error parsing the response */
+            }
+
+        };
+
+        twitch.streams().get(params, streamsResponseHandler);
+    }
 }
