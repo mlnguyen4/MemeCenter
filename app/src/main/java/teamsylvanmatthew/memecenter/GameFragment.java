57a1af644c23b6005ed24e5c7a1fd939aa839df2
Sylvan Mostert <smostert.dev@gmail.com>
2016-11-26 16:21:12 -0500

    Created simple GameFragment

---
+++ app/src/main/java/teamsylvanmatthew/memecenter/GameFragment.java
@@ -0,0 +1,16 @@
+package teamsylvanmatthew.memecenter;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View fragmentLayout = inflater.inflate(R.layout.fragment_game, container, false);
+        return fragmentLayout;
+    }
+}
