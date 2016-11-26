d3f31b371cb43f3fad8b433c402f935018f7b753
Sylvan Mostert <smostert.dev@gmail.com>
2016-11-26 00:49:31 -0500

    Added a LoadingFragment class.

---
+++ app/src/main/java/teamsylvanmatthew/memecenter/LoadingFragment.java
@@ -0,0 +1,15 @@
+package teamsylvanmatthew.memecenter;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoadingFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.loading_overlay, container, false);
+    }
+}
