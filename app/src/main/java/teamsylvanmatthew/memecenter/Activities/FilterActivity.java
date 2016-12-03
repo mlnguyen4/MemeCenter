e54494774cf39dd0f2064074d94d2d204fea3526
Sylvan Mostert <smostert.dev@gmail.com>
2016-12-03 02:45:35 -0500

    Created new Filter Activity

---
+++ app/src/main/java/teamsylvanmatthew/memecenter/Activities/FilterActivity.java
@@ -0,0 +1,15 @@
+package teamsylvanmatthew.memecenter.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import teamsylvanmatthew.memecenter.R;
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
