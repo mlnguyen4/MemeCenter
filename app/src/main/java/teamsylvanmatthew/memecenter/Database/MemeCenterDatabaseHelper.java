60f02da845b3881eb357b367108b23ceed845e62
Sylvan Mostert <smostert.dev@gmail.com>
2016-12-03 01:48:39 -0500

    Added rule table to database helper

--- app/src/main/java/teamsylvanmatthew/memecenter/Database/MemeCenterDatabaseHelper.java
+++ app/src/main/java/teamsylvanmatthew/memecenter/Database/MemeCenterDatabaseHelper.java
@@ -5,17 +5,25 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import teamsylvanmatthew.memecenter.Models.Filter;
+import teamsylvanmatthew.memecenter.Models.Rule;
 
 public class MemeCenterDatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DB_VERSION = 1;
     private static final String DB_NAME = "memecenter.db";
 
-    private static final String SQL_CREATE_TABLE = "CREATE TABLE \"" + Filter.TABLE_NAME + "\" (" + "`" + Filter.FILTER_ID_COLUMN
+    /* CREATE TABLE */
+    private static final String CREATE_TABLE_FILTER = "CREATE TABLE \"" + Filter.TABLE_NAME + "\" (" + "`" + Filter.FILTER_ID_COLUMN
             + "` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," + "`" + Filter.NAME_COLUMN + "` TEXT NOT NULL" + ");";
 
+    private static final String CREATE_TABLE_RULE = "CREATE TABLE \"" + Rule.TABLE_NAME + "\" (" + "`" + Rule.RULE_ID_COLUMN
+            + "` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," + "`" + Rule.FILTER_ID_FK_COLUMN + "` INTEGER NOT NULL," + "`" + Rule.REGEX_COLUMN + "` TEXT NOT NULL" + ");";
 
-    private static final String SQL_DELETE_QUERY = "DROP TABLE IF EXISTS " + Filter.TABLE_NAME;
+
+    /* DELETE TABLE */
+    private static final String DELETE_TABLE_FILTER = "DROP TABLE IF EXISTS " + Filter.TABLE_NAME;
+
+    private static final String DELETE_TABLE_RULE = "DROP TABLE IF EXISTS " + Rule.TABLE_NAME;
 
 
     public MemeCenterDatabaseHelper(Context context) {
@@ -23,15 +31,18 @@ public class MemeCenterDatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase database) {
         System.out.println("Executing Query: SQL_CREATE_TABLE ");
-        db.execSQL(SQL_CREATE_TABLE);
+        database.execSQL(CREATE_TABLE_FILTER);
+        database.execSQL(CREATE_TABLE_RULE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(SQL_DELETE_QUERY);
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        System.out.println("Executing Query: SQL_DELETE_TABLE ");
+        database.execSQL(DELETE_TABLE_FILTER);
+        database.execSQL(DELETE_TABLE_RULE);
+        onCreate(database);
     }
 
     @Override
