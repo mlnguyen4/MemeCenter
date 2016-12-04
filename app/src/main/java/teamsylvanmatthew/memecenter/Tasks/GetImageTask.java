9be4e0c2822d0d14c693f090fbfe18842bdc88f6
smostert <smostert.dev@gmail.com>
2016-12-04 02:55:57 -0500

    Refactored getImageTask to capital GetImageTask

---
+++ app/src/main/java/teamsylvanmatthew/memecenter/Tasks/GetImageTask.java
@@ -0,0 +1,34 @@
+package teamsylvanmatthew.memecenter.Tasks;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class GetImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView imageView;
+
+    public GetImageTask(ImageView bmImage) {
+        this.imageView = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urlDisplay = urls[0];
+
+        Bitmap mIcon = null;
+        try {
+            InputStream in = new java.net.URL(urlDisplay).openStream();
+            mIcon = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mIcon;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        imageView.setImageBitmap(result);
+    }
+}
